fc43d688a23368ac9faff95efb98ef556c166108
Tathastu App <tathastuapp@gmail.com>
2020-08-21 13:11:42 +0530

    first commit

---
+++ lib/src/controllers/home_controller.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:markets/generated/l10n.dart';
+import 'package:markets/src/models/gallery.dart';
+import 'package:markets/src/repository/gallery_repository.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
+
+import '../helpers/helper.dart';
+import '../models/category.dart';
+import '../models/market.dart';
+import '../models/product.dart';
+import '../models/review.dart';
+import '../repository/category_repository.dart';
+import '../repository/market_repository.dart';
+import '../repository/product_repository.dart';
+import '../repository/settings_repository.dart';
+
+class HomeController extends ControllerMVC {
+  Market market;
+  List<Category> categories = <Category>[];
+  List<Gallery> galleries = <Gallery>[];
+  List<Product> products = <Product>[];
+  List<Market> topMarkets = <Market>[];
+  List<Market> popularMarkets = <Market>[];
+  List<Review> recentReviews = <Review>[];
+  List<Product> trendingProducts = <Product>[];
+  GlobalKey<ScaffoldState> scaffoldKey;
+
+  HomeController() {
+    this.scaffoldKey = new GlobalKey<ScaffoldState>();
+
+    listenForMarket(id: '1');
+    listenForProducts('1');
+    listenForTopMarkets();
+    listenForGalleriesForMarket('1');
+    // listenForTrendingProducts();
+    listenForTrendingProductsForMarket('1');
+    listenForCategories();
+    listenForPopularMarkets();
+    listenForRecentReviews();
+  }
+
+  void listenForMarket({String id, String message}) async {
+    final Stream<Market> stream = await getMarket(id, deliveryAddress.value);
+    stream.listen((Market _market) {
+      setState(() => market = _market);
+    }, onError: (a) {
+      print(a);
+      scaffoldKey?.currentState?.showSnackBar(SnackBar(
+        content: Text(S.of(context).verify_your_internet_connection),
+      ));
+    }, onDone: () {
+      if (message != null) {
+        scaffoldKey?.currentState?.showSnackBar(SnackBar(
+          content: Text(message),
+        ));
+      }
+    });
+  }
+
+  void listenForProducts(String idMarket, {List<String> categoriesId}) async {
+    final Stream<Product> stream =
+        await getProductsOfMarket(idMarket, categories: categoriesId);
+    stream.listen((Product _product) {
+      setState(() => products.add(_product));
+    }, onError: (a) {
+      print(a);
+    }, onDone: () {
+      market..name = products.elementAt(0)?.market?.name;
+    });
+  }
+
+  void listenForGalleriesForMarket(String idMarket) async {
+    final Stream<Gallery> stream = await getGalleries(idMarket);
+    stream.listen((Gallery _gallery) {
+      setState(() => galleries.add(_gallery));
+    }, onError: (a) {}, onDone: () {});
+  }
+
+  Future<void> listenForCategories() async {
+    final Stream<Category> stream = await getCategories();
+    stream.listen((Category _category) {
+      setState(() => categories.add(_category));
+    }, onError: (a) {
+      print(a);
+    }, onDone: () {});
+  }
+
+  Future<void> listenForTopMarkets() async {
+    final Stream<Market> stream =
+        await getNearMarkets(deliveryAddress.value, deliveryAddress.value);
+    stream.listen((Market _market) {
+      setState(() => topMarkets.add(_market));
+    }, onError: (a) {}, onDone: () {});
+  }
+
+  Future<void> listenForPopularMarkets() async {
+    final Stream<Market> stream =
+        await getPopularMarkets(deliveryAddress.value);
+    stream.listen((Market _market) {
+      setState(() => popularMarkets.add(_market));
+    }, onError: (a) {}, onDone: () {});
+  }
+
+  Future<void> listenForRecentReviews() async {
+    final Stream<Review> stream = await getRecentReviews();
+    stream.listen((Review _review) {
+      setState(() => recentReviews.add(_review));
+    }, onError: (a) {}, onDone: () {});
+  }
+
+  Future<void> listenForTrendingProducts() async {
+    final Stream<Product> stream =
+        await getTrendingProducts(deliveryAddress.value);
+    stream.listen((Product _product) {
+      setState(() => trendingProducts.add(_product));
+    }, onError: (a) {
+      print(a);
+    }, onDone: () {});
+  }
+
+  void listenForTrendingProductsForMarket(String idMarket) async {
+    final Stream<Product> stream = await getTrendingProductsOfMarket(idMarket);
+    stream.listen((Product _product) {
+      setState(() => trendingProducts.add(_product));
+    }, onError: (a) {
+      print(a);
+    }, onDone: () {});
+  }
+
+  void requestForCurrentLocation(BuildContext context) {
+    OverlayEntry loader = Helper.overlayLoader(context);
+    Overlay.of(context).insert(loader);
+    setCurrentLocation().then((_address) async {
+      deliveryAddress.value = _address;
+      await refreshHome();
+      loader.remove();
+    }).catchError((e) {
+      loader.remove();
+    });
+  }
+
+  Future<void> refreshHome() async {
+    setState(() {
+      categories = <Category>[];
+      topMarkets = <Market>[];
+      popularMarkets = <Market>[];
+      recentReviews = <Review>[];
+      trendingProducts = <Product>[];
+    });
+    await listenForTopMarkets();
+    await listenForTrendingProducts();
+    await listenForCategories();
+    await listenForPopularMarkets();
+    await listenForRecentReviews();
+  }
+}
